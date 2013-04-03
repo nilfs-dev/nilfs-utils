1385016db7dcade070be0bc12c0cf270ef5439a7
Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
2013-04-04 00:08:11 +0900

    nilfs-utils: add autogen script
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>

---
+++ autogen.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Run this before configure
+#
+
+die() {
+    echo
+    echo "Error: $*"
+    exit 1
+}
+
+aclocal || die "aclocal failed"
+
+autoheader || die "autoheader failed" 
+
+libtoolize -c --force || die "libtoolize failed"
+
+automake -a -c || die "automake failed"
+
+echo
+echo "Creating configure..."
+
+autoconf || die "autoconf failed"
+
+echo
+echo "Now run './configure' and 'make' to compile nilfs-utils."
