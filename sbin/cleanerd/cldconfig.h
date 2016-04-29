dadf65a4fef9c6e609b4f3d90827e5fe1d33f4ce
Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
2016-04-30 01:14:36 +0900

    nilfs-utils: remove FSF mailing address from GPL notices
    As FSF address changed in the past, and can change in the future,
    remove the extra paragraph which metions FSF address in comment
    headers, and avoid errors of checkpatch.pl.
    
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>

--- sbin/cleanerd/cldconfig.h
+++ sbin/cleanerd/cldconfig.h
@@ -15,10 +15,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with NILFS; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
  * Written by Koji Sato.
  *
  * Maintained by Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp> from 2008.
