d0fdc71137e183a42c48eabc07cda231116e9d49
Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
2016-08-29 23:25:53 +0900

    compat.h: add operations on timespecs
    Add macros that give operations on timespec structures.
    
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>

--- include/compat.h
+++ include/compat.h
@@ -21,4 +21,45 @@
 #define CLOCK_BOOTTIME		7  /* Supported on Linux 2.6.39 or later */
 #endif
 
+/* Operations on timespecs */
+#ifndef timespecadd
+#define timespecadd(a, b, res)						\
+	do {								\
+		(res)->tv_sec = (a)->tv_sec + (b)->tv_sec;		\
+		(res)->tv_nsec = (a)->tv_nsec + (b)->tv_nsec;		\
+		if ((res)->tv_nsec >= 1000000000L) {			\
+			(res)->tv_sec++;				\
+			(res)->tv_nsec -= 1000000000L;			\
+		}							\
+	} while (0)
+#endif
+
+#ifndef timespecsub
+#define timespecsub(a, b, res)						\
+	do {								\
+		(res)->tv_sec = (a)->tv_sec - (b)->tv_sec;		\
+		(res)->tv_nsec = (a)->tv_nsec - (b)->tv_nsec;		\
+		if ((res)->tv_nsec < 0) {				\
+			(res)->tv_sec--;				\
+			(res)->tv_nsec += 1000000000L;			\
+		}							\
+	} while (0)
+#endif
+
+#ifndef timespecclear
+#define timespecclear(ts)						\
+	do { (ts)->tv_sec = 0; (ts)->tv_nsec = 0; } while (0)
+#endif
+
+#ifndef timespecisset
+#define timespecisset(ts)	((ts)->tv_sec || (ts)->tv_nsec)
+#endif
+
+#ifndef timespeccmp
+#define timespeccmp(a, b, cmp)						\
+	(((a)->tv_sec == (b)->tv_sec) ?					\
+	 ((a)->tv_nsec cmp (b)->tv_nsec) :				\
+	 ((a)->tv_sec cmp (b)->tv_sec))
+#endif
+
 #endif /* __COMPAT_H__ */
