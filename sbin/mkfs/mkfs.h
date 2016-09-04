4a7aba2ce2cfe9ad6a37259d1f618241b9aaf217
Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
2016-09-04 13:06:57 +0900

    nilfs2_fs.h: move nilfs_rec_len_{from,to}_disk() to mkfs.h
    Move to inline functions nilfs_rec_len_from_disk() and
    nilfs_rec_len_to_disk() into mkfs.h.
    
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@gmail.com>

--- sbin/mkfs/mkfs.h
+++ sbin/mkfs/mkfs.h
@@ -78,3 +78,25 @@ extern int ext2fs_test_bit(int nr, const void *addr);
 #define DT_SOCK		12
 #define DT_WHT		14
 
+/* Inline functions to convert record length of directory entries */
+static inline unsigned nilfs_rec_len_from_disk(__le16 dlen)
+{
+	unsigned len = le16_to_cpu(dlen);
+
+#if !defined(__KERNEL__) || (PAGE_CACHE_SIZE >= 65536)
+	if (len == NILFS_MAX_REC_LEN)
+		return 1 << 16;
+#endif
+	return len;
+}
+
+static inline __le16 nilfs_rec_len_to_disk(unsigned len)
+{
+#if !defined(__KERNEL__) || (PAGE_CACHE_SIZE >= 65536)
+	if (len == (1 << 16))
+		return cpu_to_le16(NILFS_MAX_REC_LEN);
+	else if (len > (1 << 16))
+		BUG();
+#endif
+	return cpu_to_le16(len);
+}
