nilfs-dev/nilfs-utils

commit 7aae659498b8bec410b4868c0c87e3ef8459d370

chcp: fix segfault
reported here:
https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=715729

Closes: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=715729
Signed-off-by: Ryusuke Konishi <konishi.ryusuke@gmail.com>

bin/chcp.c
@@ -116,8 +116,8 @@ int main(int argc, char *argv[])
 		dev = NULL;
 	} else {
 		modestr = argv[optind++];
-		nilfs_parse_cno(argv[optind], &endptr, CHCP_BASE);
-		if (*endptr == '\0')
+		cno = nilfs_parse_cno(argv[optind], &endptr, CHCP_BASE);
+		if (cno >= NILFS_CNO_MAX || *endptr != '\0')
 			dev = NULL;
 		else
 			dev = argv[optind++];

commit 27bfa50c448f77b8c2e5950970528b2a38216085

chcp: fix inverted logic in argument parsing
Since commit 7aae659498b8 ("chcp: fix segfault"), the logic to
distinguish between the optional device argument and the checkpoint
number list has been inverted.

The code intended to check if the current argument is *not* a valid
checkpoint number (indicating it is a device string).  However, due to
a logic error in that commit, if the argument is not a number (e.g.
"/dev/sda1"), it sets the device to NULL (implying no device argument).
Conversely, if it is a number, it incorrectly consumes it as a device
name.

This causes chcp to fail with "invalid checkpoint number" when a device
is explicitly specified, because the parser fails to advance past the
device string, subsequently attempting to parse the device name as a
checkpoint number in the main loop.

Fix this regression by correcting the if-else branch assignments.

Signed-off-by: Ryusuke Konishi <konishi.ryusuke@gmail.com>
Fixes: 7aae659498b8 ("chcp: fix segfault")

bin/chcp.c
@@ -116,9 +116,9 @@ int main(int argc, char *argv[])
 		modestr = argv[optind++];
 		cno = nilfs_parse_cno(argv[optind], &endptr, CHCP_BASE);
 		if (cno >= NILFS_CNO_MAX || *endptr != '\0')
-			dev = NULL;
-		else
 			dev = argv[optind++];
+		else
+			dev = NULL;
 	}
 
 	if (strcmp(modestr, CHCP_MODE_CP) == 0)